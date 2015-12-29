5ba511a074e93720ccd61e4ad75a23aa2d5feba1
kaadash <arturkudel@gmail.com>
2015-12-30 00:15:42 +0100

    added aeroplane controller

---
+++ src/controllers/AeroplaneController.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+/**
+ * Created by kadash on 29.12.15.
+ */
+public class AeroplaneController implements Initializable {
+    @FXML
+    private Label fuelValueLabel;
+    @FXML
+    private Label passengersValueLabel;
+    @FXML
+    private Label currentPositionLabel;
+    @FXML
+    private Label IDLabel;
+    @FXML
+    private Label currentDestinationLabel;
+    @FXML
+    private Label routeLabel;
+
+    private StringProperty text = new SimpleStringProperty(this, "text", "");
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+
+    public void updateView(int fuel, int passengersValue, Point2D currentPosition,
+                           int ID, Point2D currentDestination, ArrayList<Point> route) {
+
+        fuelValueLabel.setText(Integer.toString(fuel));
+        System.out.println(currentPosition);
+        passengersValueLabel.setText(Integer.toString(passengersValue));
+        currentPositionLabel.setText(currentPosition.toString());
+        IDLabel.setText(Integer.toString(ID));
+        currentDestinationLabel.setText(currentDestination.toString());
+//        routeLabel.setText(route.toString());
+
+    }
+
+}
--- src/meansOfTransport/Aeroplane.java
+++ src/meansOfTransport/Aeroplane.java
@@ -1,27 +1,26 @@
 package meansOfTransport;
 
-import javafx.animation.*;
+import controllers.AeroplaneController;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ResourceBundle;
 import java.util.concurrent.Semaphore;
 
-public class Aeroplane extends MeansOfTransport {
+public class Aeroplane extends MeansOfTransport{
     protected int numberOfStaff;
 
     protected int fuel;
@@ -37,7 +36,6 @@ public class Aeroplane extends MeansOfTransport {
         }
         Collections.shuffle(this.route);
         this.crossRoadPoint = new Point(550, 350);
-//        this.currentDestination = this.route.get(1);
         this.currentDestination = this.crossRoadPoint;
         this.currentPosition = this.route.get(0);
         this.fuel = 1000;
@@ -66,31 +64,20 @@ public class Aeroplane extends MeansOfTransport {
     }
 
     public void openInformationPanel() {
-        imageViewPlane.setOnMouseClicked(event -> {
-            Label fuelLabel = new Label(Integer.toString(this.getFuel()));
-            fuelLabel.setTranslateX(50);
-            Label currentDestinationLabel = new Label();
-            Label currentPositionLabel = new Label(currentPosition.toString());
-            currentDestinationLabel.setTranslateX(150);
-            currentDestinationLabel.setTranslateY(100);
-            currentPositionLabel.setTranslateX(150);
-            currentPositionLabel.setTranslateY(50);
-
-            GridPane root;
-            try {
-                root = FXMLLoader.load(getClass().getResource("aeroplaneLayout.fxml"));
-                root.getChildren().add(fuelLabel);
-                root.getChildren().add(currentDestinationLabel);
-                root.getChildren().add(currentPositionLabel);
-                currentDestinationLabel.setText(currentDestination.toString());
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            GridPane root = fxmlLoader.load(getClass().getResource("aeroplaneLayout.fxml").openStream());
+            AeroplaneController aeroplaneController = (AeroplaneController) fxmlLoader.getController();
+            aeroplaneController.updateView(fuel, numberOfStaff, currentPosition, ID, currentDestination, route);
+            imageViewPlane.setOnMouseClicked(event -> {
                 Stage stage = new Stage();
                 stage.setTitle("Aeroplane Panel");
                 stage.setScene(new Scene(root, 450, 450));
                 stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void reportIssue() {
@@ -116,10 +103,12 @@ public class Aeroplane extends MeansOfTransport {
     public void animate() {
         imageViewPlane.setLayoutX(this.getCurrentPosition().getX());
         imageViewPlane.setLayoutY(this.getCurrentPosition().getY());
+        openInformationPanel();
     }
 
     @Override
     public void run() {
+        int destinationPointer = 0;
         boolean beenIncrossRoad = false;
         double delta_x = getCurrentDestination().getX() - getCurrentPosition().getX();
         double delta_y = getCurrentDestination().getY() - getCurrentPosition().getY();
@@ -131,7 +120,7 @@ public class Aeroplane extends MeansOfTransport {
                 double currentDelta_x = getCurrentDestination().getX() - getCurrentPosition().getX();
                 double currentDelta_Y = getCurrentDestination().getY() - getCurrentPosition().getY();
                 double dist = Math.sqrt((currentDelta_x * currentDelta_x) + (currentDelta_Y * currentDelta_Y));
-                System.out.println(dist);
+//                System.out.println(dist);
 
                 if (Math.floor(dist) != 0) {
                     double ratio = speed_per_tick / goal_dist;
@@ -142,7 +131,7 @@ public class Aeroplane extends MeansOfTransport {
                     if(dist < 70 && !beenIncrossRoad && dist > 2) {
                         try {
                             aeroplaneCrossRoads.acquire();
-                            System.out.println("---- Wchodzę! ---- >>>> " + ID);
+//                            System.out.println("---- Wchodzę! ---- >>>> " + ID);
                             try {
                                 while(Math.floor(dist) >= 1){
                                     currentDelta_x = getCurrentDestination().getX() - getCurrentPosition().getX();
@@ -151,13 +140,17 @@ public class Aeroplane extends MeansOfTransport {
                                     setCurrentPosition(new Point2D.Double(x_move + getCurrentPosition().getX(),
                                             y_move + getCurrentPosition().getY()));
                                     animate();
-                                    System.out.println("bla bla " + dist);
+//                                    System.out.println("bla bla " + dist);
                                     Thread.sleep(35);
                                 }
                             }
                             finally {
-                                System.out.println("---- Wychodzę! ---- >>>> " + ID );
-                                currentDestination = route.get((int)(Math.random() * 9));
+//                                System.out.println("---- Wychodzę! ---- >>>> " + ID );
+                                if(destinationPointer == route.size() - 1) {
+                                    destinationPointer = 0;
+                                }
+                                currentDestination = route.get(destinationPointer);
+                                destinationPointer++;
                                 beenIncrossRoad = true;
                                 delta_x = getCurrentDestination().getX() - getCurrentPosition().getX();
                                 delta_y = getCurrentDestination().getY() - getCurrentPosition().getY();
@@ -168,19 +161,14 @@ public class Aeroplane extends MeansOfTransport {
                             e.printStackTrace();
                         }
                     }
-//                    System.out.println(x_move + " " + delta_x + " " + y_move + " " + delta_y);
-//                    System.out.println(dist);
-//                    System.out.println(getCurrentPosition());
 
                 } else {
-                    System.out.println("Czy wchodzę tutaj?");
+//                    System.out.println("Czy wchodzę tutaj?");
                     currentDestination = crossRoadPoint;
                     beenIncrossRoad = false;
                     delta_x = getCurrentDestination().getX() - getCurrentPosition().getX();
                     delta_y = getCurrentDestination().getY() - getCurrentPosition().getY();
                     goal_dist = Math.sqrt((delta_x * delta_x) + (delta_y * delta_y));
-
-//                    setCurrentPosition(getCurrentDestination());
                 }
                 Thread.sleep(35);
             } catch (InterruptedException e) {
@@ -190,7 +178,6 @@ public class Aeroplane extends MeansOfTransport {
                 @Override
                 public void run() {
                     animate();
-                    openInformationPanel();
                 }
             });
         }
